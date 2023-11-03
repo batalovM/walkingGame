ef5227855fe8dcf2466de45c71d4b45b2fa58f41
Batalov Maksim <117968674+batalovM@users.noreply.github.com>
2023-11-03 15:36:56 +0700

    Обновление класса статистики

--- GameClasses/Statistics.cs
+++ GameClasses/Statistics.cs
@@ -1,6 +1,34 @@
 ﻿namespace walkingGame.GameClasses;
-//cтатистика игроков
+using System;
+using System.Collections.Generic;
+
 public class Statistics
 {
-    
+    private List<Player> players;
+
+    public PlayerStatistics()
+    {
+        players = new List<Player>();
+    }
+
+    public void AddPlayer(Player player)
+    {
+        players.Add(player);
+        SortPlayers();
+    }
+
+    public void SortPlayers()
+    {
+        players.Sort((p1, p2) => p2.Score.CompareTo(p1.Score));
+    }
+
+    public void PrintStatistics()
+    {
+        Console.WriteLine("Player Statistics:");
+        for (int i = 0; i < players.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {players[i].Name} - {players[i].Score} points");
+        }
+    }
+}
 }
