4a490fb994bc5367e3a73b6d1f49ce3a85ef11fb
UglyGod46 <patlasov.deniska@mail.ru>
2023-11-03 13:59:12 +0300

    update cell

--- GameClasses/Cell.cs
+++ GameClasses/Cell.cs
@@ -2,5 +2,28 @@
 
 public class Cell
 {
-    
+    public int Number { get; set; }
+
+    public Cell(int number)
+    {
+        Number = number; // Клетка с номером
+    }
+
+    public Cell()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SkipTurn()
+    {
+        // Реализация пропуска хода
+    }
+    public void ExtraTurn()
+    {
+        // Реализация дополнительного хода
+    }
+    public void LoseTurn()
+    {
+        // Реализация потери хода
+    }
 }
