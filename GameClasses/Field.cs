e9794ea5c57d2b10ee3524c4afea18152eaff90d
batalovM <b.maksim1708@gmail.com>
2023-11-03 15:01:58 +0700

    test

--- GameClasses/Field.cs
+++ GameClasses/Field.cs
@@ -1,6 +1,22 @@
-﻿namespace walkingGame.GameClasses;
+﻿using System.Collections.Generic;
+
+namespace walkingGame.GameClasses;
 
 public class Field
 {
+    private Dictionary<int, Cell> field = new();
+
+    private void UpdateForGame(Dictionary<int, Cell> field)
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            this.field.Add(i, new Cell());
+        }
+    }
+    
+    private void ChangeStateOfCell(Dictionary<int, Cell> field, int key)
+    {
+        
+    }
     
 }
