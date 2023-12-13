98432fc7d8557cf5a4ec25750b4d256468ae835b
batalovM <b.maksim1708@gmail.com>
2023-12-13 15:08:07 +0700

    upt

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,48 +1,51 @@
 ﻿using System;
 using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Media.Imaging;
 using ReactiveUI;
 using walkingGame.GameClasses.Model;
-
 namespace walkingGame.ViewModels;
-
 public class MainWindowViewModel : ViewModelBase
 {
-    private readonly Person _player1;
-    private readonly Person _player2;
-    private Cell _cellFirstPlayer;
-    private Cell _cellSecondPlayer;
-    private bool _isPlayer1Turn = true;
-    private string _playerTurn = "Ход игрока 1";
-    public string PlayerTurn
-    {
-        get => _playerTurn;
-        set
-        {
-            _playerTurn = value;
-            this.RaisePropertyChanged();
-        }
-    }
-    public int FirstPlayerScore => _player1.Score;
-    public int SecondPlayerScore => _player2.Score;
-    public Field GameField { get; set; }
-    public ReactiveCommand<Unit, Unit> CellClickedButton { get; }
     public MainWindowViewModel()
     {
         GameField = new Field(); 
         GameField.UpdateforGame();
         _player1 = new Person(1);
         _player2 = new Person(1);
+        ImagePlayer1 = new Bitmap(@"C:\Users\batal\RiderProjects\walkingGame\Assets\copy.png");
+        ImagePlayer2 = new Bitmap(@"C:\Users\batal\RiderProjects\walkingGame\Assets\copy_copy.png");
+        ImageMargin = new Thickness(0, 0, 0, 0);
         CellClickedButton = ReactiveCommand.Create(MakeMove);
         _cellFirstPlayer = new Cell(_player1.Score, GameField.GetCell(_player1.Score));
         _cellSecondPlayer = new Cell(_player2.Score, GameField.GetCell(_player2.Score));
     }
-
-    private void MakeMove()
+    private Thickness _imageMargin;//margin 1 игрока
+    private Bitmap _imagePlayer1;//каринка 1 игрока
+    private Bitmap _imagePlayer2;//каринка 2 игрока
+    private readonly Person _player1;
+    private readonly Person _player2;
+    private readonly Cell _cellFirstPlayer;//текущая клетка 1 игрока
+    private readonly Cell _cellSecondPlayer;//текущая клетка 2 игрока
+    private bool _isPlayer1Turn = true;
+    private string _playerTurn = "Ход игрока 1";
+    public Thickness ImageMargin { get => _imageMargin;set => this.RaiseAndSetIfChanged(ref _imageMargin, value);}//изменение margin для передвижения 1 игрока
+    public Bitmap ImagePlayer1 {get => _imagePlayer1; set => this.RaiseAndSetIfChanged(ref _imagePlayer1, value);}//получение картинки 1 игрока
+    public Bitmap ImagePlayer2 {get => _imagePlayer2; set => this.RaiseAndSetIfChanged(ref _imagePlayer2, value); }//получение картинки 2 игрока
+    public string PlayerTurn {get => _playerTurn; set{ _playerTurn = value; this.RaisePropertyChanged();}}//получение хода игрока 
+    public int FirstPlayerScore => _player1.Score;//количество очков 1 игрока
+    public int SecondPlayerScore => _player2.Score;//количество очков 2 игрока
+    public Field GameField { get; set; }//игровое поле 
+    public ReactiveCommand<Unit, Unit> CellClickedButton { get; }//команда для нажатия кнопка
+    private void MakeMove()//метод игры 
     {
         if (_player1.Score >= 32 ^ _player2.Score >= 32) return;
         if (_isPlayer1Turn)
         {
             AffectCell(_cellFirstPlayer, _player1);
+            ImageMargin = new Thickness(ImageMargin.Right + _player1.Score*150-150, ImageMargin.Top, ImageMargin.Right, ImageMargin.Bottom); 
             Console.WriteLine(_player1.Score);
             if(_cellFirstPlayer.Type == CellType.Normal) _cellFirstPlayer.NormalTurn();
             if(_cellFirstPlayer.Type == CellType.BackTurn) _cellFirstPlayer.BackTurn(_player1);
@@ -63,12 +66,12 @@ public class MainWindowViewModel : ViewModelBase
             PlayerTurn = "Ход игрока 1";
         }
         _isPlayer1Turn = !_isPlayer1Turn;
-        this.RaisePropertyChanged(nameof(FirstPlayerScore));
+        this.RaisePropertyChanged(nameof(FirstPlayerScore));//обновление количества очков 
         this.RaisePropertyChanged(nameof(SecondPlayerScore));
     }
     public void AffectCell(Cell cell, Person person)
     {
         cell.Affect(person);
     }
-
+    
 }
